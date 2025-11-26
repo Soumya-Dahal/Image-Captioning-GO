40968ef6b02a6dcc628d53d6a7c42e06bc7b33a7
Nitesh Kamat <nitesh0434@gmail.com>
2025-11-27 01:46:08 +0545

    Fix CORS port and update requirements for compatibility by removing strict version

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ type ErrorResponse struct {
 // Custom CORS middleware
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3001")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
